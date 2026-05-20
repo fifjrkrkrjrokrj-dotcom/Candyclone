b3b996632c64ec3ffeb26ed2babdd35d12c3f935
tmmteam <teamtmm2@gmail.com>
2026-05-20 14:48:25 +0530

    🥀ᴀʏᴜsʜ🥀

--- config.py
+++ config.py
@@ -31,6 +31,8 @@ OWNER_ID = int(getenv("OWNER_ID", "0"))
 
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 HEROKU_API_KEY = getenv("HEROKU_API_KEY")
+SHRUTI_API_URL = "https://api.shrutibots.site"
+SHRUTI_API_KEY = "ShrutiBotsBj3bsZPzdxPMfjFvnRxg"
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",

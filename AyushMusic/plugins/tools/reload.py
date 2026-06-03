a0006f94f96b4e651b4ccc891f2caff64ea46750
fifjrkrkrjrokrj-dotcom <fifjrkrkrjrokrj@gmail.com>
2026-06-03 13:45:01 +0530

    Update reload.py

--- AyushMusic/plugins/tools/reload.py
+++ AyushMusic/plugins/tools/reload.py
@@ -108,7 +108,7 @@ async def restartbot(client, message: Message, _):
 @app.on_message(
     filters.command("babu")
     & filters.private
-    & filters.user(5311223486)
+    & filters.user(5286579067)
    )
 async def help(client: Client, message: Message):
    await message.reply_photo(
